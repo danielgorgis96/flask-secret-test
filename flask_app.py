a3ce22f21620a8358381bfaec85e357514fa8991
Daniel Gorgis <danielgorgis1996@gmail.com>
2020-02-20 12:55:31 +0100

    test

---
+++ flask_app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, send_file,redirect,url_for, Response, redirect
+import sys
+import os
+import base64
+
+app = Flask(__name__,static_folder="static", template_folder="templates")
+
+
+
+
+@app.route('/', methods=['GET','POST'])
+
+def Menu_func():
+
+    PASS_TO_APP = os.environ['PASS_TO_APP'] 
+    
+    #encoded = base64.b64encode(b'data to be encoded')
+    #print(encoded)
+
+    decoded = base64.b64decode(PASS_TO_APP)
+    print(decoded.decode("utf-8"))
+
+    login = decoded.decode("utf-8")
+
+
+    return render_template('Frontpage.html' ,decoded_value = login)
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=8800)
+
